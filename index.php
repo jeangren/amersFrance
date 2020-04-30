189d359f4fa2436837c8e75f6b2340f0ff66a988
Jean Grenouiller <jeangren@free.fr>
2020-04-30 22:15:46 +0200

    foreach avec images

--- index.php
+++ index.php
@@ -18,62 +18,49 @@ $request = "SELECT * FROM navigation.amers";
 $response = $bdd->query($request);
 $navigation = $response->fetchAll(PDO::FETCH_ASSOC);
 //  var_dump($navigation);
- ?>
 
-<body>
- <?php include 'header.php' ?>
+include 'header.php';
+?>
 
-    <div class="jumbotron text-center">
-        <h1 class="display-4">Les différents amers à travers le monde</h1>
-        <p class="lead">Ce site à pour but de référencer les différents amers à travers le monde</p>
-        <hr class="my-4">
-        <h6>Vous pouvez ajouter des amers que vous avez découvert et photographié lors de vos sorties en mer</h6>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Ajouter un amer</a>
-    </div>
 
-    <div class="container">
 
-        <div class="row">
 
-            <div class="card-deck">
+<div class="jumbotron text-center">
+    <h2 class="display-4">AMER [amε:ʀ] : Point de repère utilisé pour la navigation maritime.</h2>
+    <p class="lead">Découvrez une multitude d'amers à travers le monde</p>
+    <hr class="my-4">
+    <h6>Vous pouvez ajouter des amers que vous avez découverts et photographiés lors de vos sorties en mer</h6>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Ajouter un amer</a>
+</div>
+
+<div class="container">
+
+    <div class="row">
+        <?php foreach ($navigation as $amer) : ?>
+            <div class="card-deck col-md-4 m-4">
                 <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="img/<?= $amer['IMG_SRC'] ?>" class="card-img-top" alt="amer">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <h5 class="card-title"><?= $amer['NOM'] ?></h5>
+                        <p class="card-text"><?= $amer['PAYS'] . " " . $amer['REGION'] ?></p>
+                        <p class="card-text"><small class="text-muted"><?= $amer['COMMENTAIRE']?></small></p>
                     </div>
                 </div>
             </div>
+        <?php endforeach ?>
 
-        </div>
     </div>
+</div>
 
 
-    <?php include 'footer.php' ?>
+<?php include 'footer.php' ?>
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
