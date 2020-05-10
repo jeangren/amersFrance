1aae4b5bbde64174e2e8343013d9e88a5c2ecbb5
Jean Grenouiller <jeangren@free.fr>
2020-05-10 12:33:41 +0200

    opensession ok

--- opensession.php
+++ opensession.php
@@ -30,22 +30,32 @@ $resultat = $response->fetch();
 // Comparaison du pass envoyé via le formulaire avec la base
 $isPasswordCorrect = password_verify($_POST['password'], $resultat['password']);
 
-if (!$resultat)
-{
+// if (!$resultat)
+// {
+//     echo 'Mauvais identifiant ou mot de passe !';
+// }
+// else
+// {
+//     if ($isPasswordCorrect) {
+//         session_start();
+//         $_SESSION['id'] = $resultat['id'];
+//         $_SESSION['pseudo'] = $_POST['pseudo'];
+//         echo 'Vous êtes connecté !';
+//     }
+    
+// }
+
+if ($isPasswordCorrect) {
+    session_start();
+    $_SESSION['id'] = $resultat['id'];
+    $_SESSION['pseudo'] = $_POST['pseudo'];
+    echo 'Bonjour ' .$_POST['pseudo'] . ' Vous êtes connecté !';
+}
+else{
     echo 'Mauvais identifiant ou mot de passe !';
 }
-else
-{
-    if ($isPasswordCorrect) {
-        session_start();
-        $_SESSION['id'] = $resultat['id'];
-        $_SESSION['pseudo'] = $_POST['pseudo'];
-        echo 'Vous êtes connecté !';
-    }
-    else {
-        echo 'Mauvais identifiant ou mot de passe !';
-    }
-}
+
+
 
 ?>
 
